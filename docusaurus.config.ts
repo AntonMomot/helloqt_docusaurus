ba209fb56fef07f8e53c7d930c11a11f840c6eba
AntonMomot <anton.momot@bergx2.de>
2024-05-27 14:20:41 +0300

    save config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,11 @@ const config: Config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://github.com/AntonMomot/helloqt_docusaurus",
+    url: "https://AntonMomot.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/helloqt_docusaurus/",
+    deploymentBranch: 'main',
 
     trailingSlash: false,
     onBrokenLinks: "throw",
