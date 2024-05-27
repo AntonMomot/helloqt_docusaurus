18c751a10f5548dc83bc4145f23eace3f512b354
AntonMomot <anton.momot@bergx2.de>
2024-05-27 15:31:59 +0300

    add workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
     url: "https://AntonMomot.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/helloqt_docusaurus/docs/",
+    baseUrl: "/helloqt_docusaurus/",
     deploymentBranch: 'main',
 
     trailingSlash: false,
